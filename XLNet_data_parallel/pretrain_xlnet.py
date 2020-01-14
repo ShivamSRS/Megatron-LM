5e66dd71294b920052566204510a8b519bb99428
Cdivyam <divyamchoudhary@hotmail.com>
2020-01-14 04:31:27 +0000

    XLNet Data parallel init

---
+++ XLNet_data_parallel/pretrain_xlnet.py
@@ -0,0 +1,121 @@
+from datetime import datetime
+import os
+import random
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from arguments import get_args
+from configure_data import configure_data
+from fp16 import FP16_Module
+from fp16 import FP16_Optimizer
+from learning_rates import AnnealingLR
+from model import BertModel
+from model import get_params_for_weight_decay_optimization
+from model import gpt2_get_params_for_weight_decay_optimization
+from model import DistributedDataParallel as LocalDDP
+import mpu
+from apex.optimizers import FusedAdam as Adam
+from utils import Timers
+from utils import save_checkpoint
+from utils import load_checkpoint
+from utils import report_memory
+from utils import print_args
+from utils import print_params_min_max_norm
+from utils import print_rank_0
+from utils import enable_adlr_autoresume
+from utils import check_adlr_autoresume_termination
+
+def initialize_distributed(args):
+    """Initialize torch.distributed."""
+
+    # Manually set the device ids.
+    device = args.rank % torch.cuda.device_count()
+    if args.local_rank is not None:
+        device = args.local_rank
+    torch.cuda.set_device(device)
+    # Call the init process
+    init_method = 'tcp://'
+    master_ip = os.getenv('MASTER_ADDR', 'localhost')
+    master_port = os.getenv('MASTER_PORT', '6000')
+    init_method += master_ip + ':' + master_port
+    torch.distributed.init_process_group(
+        backend=args.distributed_backend,
+        world_size=args.world_size, rank=args.rank,
+        init_method=init_method)
+
+    # Set the model-parallel / data-parallel communicators.
+    mpu.initialize_model_parallel(args.model_parallel_size)
+
+def set_random_seed(seed):
+    """Set random seed for reproducability."""
+
+    if seed is not None and seed > 0:
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        mpu.model_parallel_cuda_manual_seed(seed)
+
+        
+def get_train_val_test_data(args):
+    """Load the data on rank zero and boradcast number of tokens to all GPUS."""
+
+    (train_data, val_data, test_data) = (None, None, None)
+
+    # Data loader only on rank 0 of each model parallel group.
+    if mpu.get_model_parallel_rank() == 0:
+        data_config = configure_data()
+        data_config.set_defaults(data_set_type='BERT', transpose=False)
+        (train_data, val_data, test_data), tokenizer = data_config.apply(args)
+        before = tokenizer.num_tokens
+        after = before
+        multiple = args.make_vocab_size_divisible_by * \
+                   mpu.get_model_parallel_world_size()
+        while (after % multiple) != 0:
+            after += 1
+        print_rank_0('> padded vocab (size: {}) with {} dummy '
+                     'tokens (new size: {})'.format(
+                         before, after - before, after))
+        # Need to broadcast num_tokens and num_type_tokens.
+        token_counts = torch.cuda.LongTensor([after,
+                                              tokenizer.num_type_tokens,
+                                              int(args.do_train), int(args.do_valid), int(args.do_test)])
+    else:
+        token_counts = torch.cuda.LongTensor([0, 0, 0, 0, 0])
+    
+    # Broadcast num tokens.
+    torch.distributed.broadcast(token_counts,
+                                mpu.get_model_parallel_src_rank(),
+                                group=mpu.get_model_parallel_group())
+    num_tokens = token_counts[0].item()
+    num_type_tokens = token_counts[1].item()
+    args.do_train = token_counts[2].item()
+    args.do_valid = token_counts[3].item()
+    args.do_test = token_counts[4].item()
+
+    return train_data, val_data, test_data, num_tokens, num_type_tokens
+
+def main():
+    """Main training program."""
+    print("I am here_main")
+    # Disable CuDNN.
+    torch.backends.cudnn.enabled = False
+    
+    # Timer.
+    timers = Timers()
+    
+    # Arguments.
+    args = get_args()
+    
+    # Pytorch distributed.
+    initialize_distributed(args)
+    if torch.distributed.get_rank() == 0:
+        print('Pretrain XLNet model')
+        print_args(args)
+    
+    # Random seeds for reproducability.
+    set_random_seed(args.seed)
+    
+    train_data, val_data, test_data, args.tokenizer_num_tokens, \
+        args.tokenizer_num_type_tokens = get_train_val_test_data(args)
+    
